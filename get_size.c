f687ebf07d26f0e23fe4f40769537ea3c7a3c618
thebarmy <souiyahk99@gmail.com>
2023-10-18 14:51:42 -0700

    commit

--- get_size.c
+++ get_size.c
@@ -9,18 +9,21 @@
  */
 int get_size(const char *format, int *i)
 {
-   int present_i;
-   present_i = (*i) + 1;
-   int size = 0;
+int present_i;
+int size = 0;
 
-    if (format[present_i] == 'l') {
-    size = S_LONG;
-} else if (format[present_i] == 'h') {
-    size = S_SHORT;
+present_i = (*i) + 1;
+
+if (format[present_i] == 'l')
+{
+size = S_LONG;
+}
+else if (format[present_i] == 'h')
+{
+size = S_SHORT;
 }
 
 *i = (size == 0) ? present_i - 1 : present_i;
 
-
-    return size;
+return (size);
 }
